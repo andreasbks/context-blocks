b583c53e24d926abd1aaa19db5d623b4efe26666
Andreas <72885357+andreasbks@users.noreply.github.com>
2025-08-17 13:34:15 +0100

    Update page.tsx
    Removed redirect url as its in env

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <SignUp routing="hash" signInUrl="/auth/login" />
+        <SignUp routing="hash" />
       </div>
     </div>
   );
